e1f02a2fdb0453d79824763b3bc2f46363046f95
Martin Høy <marhoy@gmail.com>
2020-02-25 19:59:45 +0100

    Better implementation of register encode/decode

--- src/pyflexit/common.py
+++ src/pyflexit/common.py
@@ -1,35 +1,59 @@
+import struct
 from abc import ABC, abstractmethod
-from typing import Tuple, List
-
-from pymodbus.constants import Endian
-from pymodbus.payload import BinaryPayloadDecoder, BinaryPayloadBuilder
+from typing import Iterable, Tuple
 
 
-def registers_to_value(registers: List[int], data_type: str):
-    """Decode a list of registers to a value, given the specified data type.
+def zero_pad(data_format: str) -> str:
+    """Data formats smaller than 16 bits must be zero-padded
+
+    Args:
+        data_format (str): A single data format character, see struct module
+
+    Returns:
+        A possible zero-padded data format string
+
+    Examples:
+        A float doesn't need zero-padding
+        >>> zero_pad("f")
+        'f'
+
+        A boolean is only one byte, and must be zero-padded
+        >>> zero_pad("?")
+        'x?'
+    """
+    if struct.calcsize(data_format) < 2:
+        return f"x{data_format}"
+    return data_format
+
+
+def registers_to_values(registers: Iterable[int], data_types: str):
+    """Decode a list of registers to value(s), given the specified data type(s).
 
     The registers argument will typically be the .registers attribute of a
     pymodbus response object.
 
     Args:
         registers (List[int]): List of register values
-        data_type (str): See the struct module for examples
+        data_types (str): See the struct module for examples
 
     Returns:
-        The decoded value
+        A tuple with the decoded value(s)
+
+    Examples:
+        Decoding a 32-bit float from two registers:
+        >>> registers_to_values((17562, 20480), 'f')
+        (1234.5,)
+
+        Decoding multiple values of different type from multiple registers:
+        >>> registers_to_values([17562, 20480, 1, 2, 1, 17833, 47104], "fi?f")
+        (1234.5, 65538, True, 5431.0)
     """
-    decoder = BinaryPayloadDecoder.fromRegisters(
-        registers, byteorder=Endian.Big)
-    if data_type == 'f':
-        return decoder.decode_32bit_float()
-    if data_type == 'I':
-        return decoder.decode_32bit_uint()
-    if data_type == 'H':
-        return decoder.decode_16bit_int()
-    raise ValueError(f"Unknown data type: {data_type}")
+    binary_format = "".join(zero_pad(data_type) for data_type in data_types)
+    byte_string = b''.join(struct.pack('>H', r) for r in registers)
+    return struct.unpack(f">{binary_format}", byte_string)
 
 
-def value_to_registers(value, data_type: str) -> List[int]:
+def value_to_registers(value, data_type: str) -> Tuple:
     """Encode a value into a list of registers.
 
     The resulting list of registers can be written to an address:
@@ -40,18 +64,16 @@ def value_to_registers(value, data_type: str) -> List[int]:
         data_type (str): The data type, see struct module for examples
 
     Returns:
-        The list of registers
+        A tuple of registers
+
+    Example:
+        Encoding a 32-bit float into two registers:
+        >>> value_to_registers(1234.5, 'f')
+        (17562, 20480)
     """
-    encoder = BinaryPayloadBuilder(byteorder=Endian.Big)
-    if data_type == 'f':
-        encoder.add_32bit_float(value)
-    elif data_type == 'I':
-        encoder.add_32bit_uint(value)
-    elif data_type == 'H':
-        encoder.add_16bit_int(value)
-    else:
-        ValueError(f"Unknown data type: {data_type}")
-    return encoder.to_registers()
+    binary_format = zero_pad(data_type)
+    bs = struct.pack(f">{binary_format}", value)
+    return struct.unpack(f">{struct.calcsize(binary_format) // 2}H", bs)
 
 
 class HomeAssistantAPI(ABC):
